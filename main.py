9cf36c7dcf867d3005364724664abae75a538ffd
Dominik Rosiek <45dominik@gmail.com>
2014-06-18 09:12:24 +0000

    Wersja prawie końcowa, zebranie całej pracy

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3.3
+# -*- coding: utf-8 -*-
+
+"""
+Graficzna aplikacja SEFR:
+Autorzy: Dominik Rosiek & Piotr Ścibor
+"""
+print("Importowanie bibliotek");
+from gui import *;
+print("Biblioteki zaimportowano")
+
+def main():
+    numpy.set_printoptions(threshold= 10e40)
+    app = QtGui.QApplication(sys.argv)
+    Ap = SEFR_GUI()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()   
+ 
---
+++ psnr.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3.3
+import os;
+import sys;
+from subprocess import *
+from PIL import Image			#Moduł do przetwarzania obrazów
+os.system("./main.py lena/lena.pgm lena/out.pgm znak")
+os.system(">lena%s.txt" %sys.argv[1])
+ile = int(sys.argv[1])
+for i in range(ile+1):
+	print(i)
+	im = Image.open("lena/out.pgm")
+	data = list(im.getdata())
+	for j in range(int(256*256/ile*i)):
+		data[j] = 0;
+	newimg = Image.new("L", im.size) 
+	newimg.putdata(data)
+
+	newimg.save("lena/out2.pgm")
+	os.system("./main.py lena/out2.pgm lena/out2.pgm recover")
+	os.popen("compare -metric PSNR lena/lena.pgm lena/out2.pgm lena/diff%s.pgm 2>> lena%s.txt" %(i, sys.argv[1])).readlines();
